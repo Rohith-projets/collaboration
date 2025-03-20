c6110af8cc6593a0d603168231884eb64dffccbc
Rohith <educationalhub25012003@gmail.com>
2025-03-20 11:41:47 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -36,10 +36,10 @@ class Collaboration:
                     with col2:
                         team_info = sent_data[team_index]
                         st.subheader(f"Data for {selected_team}")
-                        st.text(f"Team members : {team_info['team members']}")
+
                         for key, value in team_info.items():
                             if key.startswith("data_"):
-                                st.subheader(value['key'],divider='blue')
+                                st.write(f"**{value['key']}**")
                                 st.text(value['description'])
                                 st.dataframe(pd.DataFrame(value['data']))
             else:
@@ -50,24 +50,61 @@ class Collaboration:
         with col1:
             sender_email = st.text_input("Enter sender's email")
             receiver_email = st.text_input("Enter your email")
-            collaboration_date = st.date_input("Select collaboration date")
+            selected_date = st.date_input("Select Collaboration Date")
 
-        if sender_email and receiver_email and collaboration_date:
+        if sender_email and receiver_email and selected_date:
             received_data = list(self.collection.find({
                 "sender": sender_email,
                 "team members": {"$in": [receiver_email]},
-                "date": str(collaboration_date)
+                "date": str(selected_date)
             }))
 
             if received_data:
-                with col2:
-                    st.subheader(f"Data received from {sender_email} on {collaboration_date}")
-                    for data_entry in received_data:
-                        for key, value in data_entry.items():
+                team_options = [f"Team {idx + 1}" for idx in range(len(received_data))]
+                selected_team = col1.radio("Teams you are part of", team_options)
+
+                if selected_team:
+                    team_index = int(selected_team.split()[1]) - 1
+                    with col2:
+                        team_info = received_data[team_index]
+                        st.subheader(f"Data received from {sender_email}")
+
+                        for key, value in team_info.items():
                             if key.startswith("data_"):
                                 st.write(f"**{value['key']}**")
                                 st.text(value['description'])
                                 st.dataframe(pd.DataFrame(value['data']))
+
+                        # Comment Section
+                        st.markdown("### Add a Comment")
+                        comment = st.text_area("Write your comment")
+                        if st.button("Submit Comment", use_container_width=True):
+                            if comment.strip():
+                                comments = team_info.get("comments", {})
+                                if receiver_email in comments:
+                                    comments[receiver_email].append(comment)
+                                else:
+                                    comments[receiver_email] = [comment]
+
+                                self.collection.update_one(
+                                    {"_id": team_info["_id"]},
+                                    {"$set": {"comments": comments}}
+                                )
+                                st.success("Comment added successfully!")
+                            else:
+                                st.warning("Please enter a valid comment.")
+
+                        # Display Comments
+                        st.markdown("### Comments")
+                        comments = team_info.get("comments", {})
+                        if comments:
+                            for member, comments_list in comments.items():
+                                st.write(f"**{member}:**")
+                                for cmnt in comments_list:
+                                    st.text(f"- {cmnt}")
+                        else:
+                            st.text("No comments available.")
+
             else:
                 col1.warning("No data found matching this combination.")
 collab = Collaboration()
